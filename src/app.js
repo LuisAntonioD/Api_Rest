65e1a415ccb5e930226ab007dde1a93102cbda22
Luis Antonio Arredondo Deanda <99154912+LuisAntonioD@users.noreply.github.com>
2024-05-29 22:32:20 -0600

    Update app.js

--- src/app.js
+++ src/app.js
@@ -9,12 +9,7 @@ const app = express();
 createRoles();
 
 
-// Configurar CORS
-const corsOptions = {
-origin: http://localhost:5173,
-methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-allowedHeaders: 'Content-Type,Authorization',
-};
+
 //export default app;
 app.use(express.json());
 
