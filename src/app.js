ccce7cca031dbb7574892e70827b6df82fc42043
Luis Antonio Arredondo Deanda <99154912+LuisAntonioD@users.noreply.github.com>
2024-05-29 22:28:25 -0600

    Update app.js

--- src/app.js
+++ src/app.js
@@ -2,10 +2,19 @@ import express from "express";
 import productsRoutes from "../src/routes/products.routes";
 import authRoutes from "./routes/auth.routes";
 import createRoles from "./libs/initialSetup";
+import cors from 'cors';
+
 const app = express();
 //Ejecutar la función para crear roles por defecto hola
 createRoles();
 
+
+// Configurar CORS
+const corsOptions = {
+origin: http://localhost:5173,
+methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+allowedHeaders: 'Content-Type,Authorization',
+};
 //export default app;
 app.use(express.json());
 
